3de74190996b38082a0ab01817f02f16257ada50
sang-w0o <robbyra@gmail.com>
2021-09-29 21:05:07 +0900

    feat: Apply HttpSecurityDSL in SecurityConfig

--- src/main/kotlin/com/template/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/template/security/config/SecurityConfig.kt
@@ -3,48 +3,49 @@ package com.template.security.config
 import com.template.auth.tools.JwtTokenUtil
 import com.template.security.filter.JWTRequestFilter
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
-@Configuration
 @EnableWebSecurity
 class SecurityConfig(private val jwtTokenUtil: JwtTokenUtil, private val authenticationEntryPoint: AuthenticationEntryPoint) : WebSecurityConfigurerAdapter() {
 
-    override fun configure(http: HttpSecurity?) {
+    override fun configure(http: HttpSecurity) {
         // TODO: JWT 인증 절차를 추가할 End point 추가 및 수정
-        http!!
-        http
-            .httpBasic().disable()
-            .headers().frameOptions().disable()
-            .and()
-            .csrf().disable()
-            .cors().configurationSource(corsConfigurationSource())
-            .and()
-            .logout().disable()
-            .formLogin().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-            .antMatchers("/v1/**")
-            .authenticated()
-            .and()
-            .addFilterBefore(JWTRequestFilter(jwtTokenUtil, authenticationEntryPoint), UsernamePasswordAuthenticationFilter::class.java)
-            .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+        http {
+            httpBasic {
+                disable()
+            }
+            headers {
+                frameOptions { disable() }
+            }
+            csrf { disable() }
+            cors { configurationSource = corsConfigurationSource() }
+            logout { disable() }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+            authorizeRequests {
+                authorize("/v1/**", authenticated)
+            }
+            addFilterBefore(JWTRequestFilter(jwtTokenUtil, authenticationEntryPoint), UsernamePasswordAuthenticationFilter::class.java)
+            exceptionHandling {
+                authenticationEntryPoint
+            }
+        }
     }
 
-    override fun configure(web: WebSecurity?) {
+    override fun configure(web: WebSecurity) {
         // TODO: JWT 인증 절차를 제외할 End point 추가 및 수정
-        web!!
         web.ignoring()
             .mvcMatchers(HttpMethod.POST, "/v1/auth/update-token")
             .mvcMatchers(HttpMethod.POST, "/v1/auth/login")
