cf20bedc11b57de45532220a71a4c1dbfd8cabd7
sang-w0o <robbyra@gmail.com>
2021-11-05 11:11:02 +0900

    chore: Add com.ninja-squad:springmockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.0.0.M3")
     testImplementation("io.kotest:kotest-assertions-core:5.0.0.M3")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
     testImplementation("com.h2database:h2")
     testImplementation("org.apache.httpcomponents:httpclient:4.5.13")
 }
